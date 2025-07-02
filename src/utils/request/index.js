db7ee236397fecda3316bcf2f5a0f84c90f1f9b5
qizhang01 <zhishui120@sina.com>
2025-07-02 19:12:18 +0800

    删除多余功能

--- src/utils/request/index.js
+++ src/utils/request/index.js
@@ -11,30 +11,16 @@ const http = new Request();
 http.setConfig(config => {
 	/* 设置全局配置 */
 	config.baseUrl = indexConfig.baseUrl; /* 根域名不同 */
-	const systemInfo = uni.getSystemInfoSync();
-	const systemInfoHeaders = {
-		API_KEY: "bpQ7]qnAzwe1,HQ74Zt4V(cRf2v[m,",
-		'device-name': systemInfo.brand, // 设备名称
-		width: systemInfo.screenWidth, // 屏幕宽度
-		height: systemInfo.screenHeight, // 屏幕高度
-		os: systemInfo.platform, // 客户端平台
-		'os-version': systemInfo.system // 操作系统版本
-	};
 	config.header = {
 		...config.header,
-		...systemInfoHeaders
+		API_KEY: "bpQ7]qnAzwe1,HQ74Zt4V(cRf2v[m,",
 	};
 	return config;
 });
 
-let isRefreshing = false;
-let requests = [];
+
 http.interceptor.request(
 	config => {
-		/* 请求之前拦截器 */
-		// config.header['x-api-key'] = uni.getStorageSync('accessToken');
-		// 单商户
-		// config.header['merchant-id'] = uni.getStorageSync('merchantId') || 1;
 		return config;
 	},
 	error => {
@@ -42,16 +28,7 @@ http.interceptor.request(
 	}
 );
 
-// 刷新refreshToken
-async function handleRefreshToken(refresh_token) {
-	// const params = {};
-	// params.group = mHelper.platformGroupFilter();
-	// params.refresh_token = refresh_token;
-	// await http.post(refreshToken, params).then(async r => {
-	// 	store.commit('login', r.data);
-	// 	isRefreshing = false;
-	// });
-}
+
 http.interceptor.response(
 	async response => {
 		/* 请求之后拦截器 */
--- src/utils/request/request.js
+++ src/utils/request/request.js
@@ -1,12 +1,3 @@
-/**
- * Request 1.0.6
- * @Class Request
- * @description luch-request 1.0.6 http请求插件
- * @Author lu-ch
- * @Date 2020-03-17
- * @Email webwork.s@qq.com
- * http://ext.dcloud.net.cn/plugin?id=392
- */
 export default class Request {
 	config = {
 		baseUrl: '',
@@ -15,42 +6,17 @@ export default class Request {
 		},
 		method: 'GET',
 		dataType: 'json',
-		// #ifndef MP-ALIPAY || APP-PLUS
-		responseType: 'text',
-		// #endif
-		custom: {},
-		// #ifdef MP-ALIPAY
-		timeout: 30000,
-		// #endif
 		// #ifdef APP-PLUS
-		sslVerify: true
+		sslVerify: true,
 		// #endif
+		API_KEY: "bpQ7]qnAzwe1,HQ74Zt4V(cRf2v[m,",
 	};
 
-	static posUrl(url) {
-		/* 判断url是否为绝对路径 */
-		return /(http|https):\/\/([\w.]+\/?)\S*/.test(url);
-	}
-
-
-	/**
-	 * @property {Function} request 请求拦截器
-	 * @property {Function} response 响应拦截器
-	 * @type {{request: Request.interceptor.request, response: Request.interceptor.response}}
-	 */
 	interceptor = {
-		/**
-		 * @param {Request~requestCallback} cb - 请求之前拦截,接收一个函数（config, cancel）=> {return config}。第一个参数为全局config,第二个参数为函数，调用则取消本次请求。
-		 */
 		request: cb => {
-			if (cb) {
-				this.requestBeforeFun = cb;
-			}
+
 		},
-		/**
-		 * @param {Request~responseCallback} cb 响应拦截器，对响应数据做点什么
-		 * @param {Request~responseErrCallback} ecb 响应拦截器，对响应错误做点什么
-		 */
+
 		response: (cb, ecb) => {
 			if (cb) {
 				this.requestComFun = cb;
@@ -73,68 +39,29 @@ export default class Request {
 		return response;
 	}
 
-	/**
-	 * 自定义验证器，如果返回true 则进入响应拦截器的响应成功函数(resolve)，否则进入响应拦截器的响应错误函数(reject)
-	 * @param { Number } statusCode - 请求响应体statusCode（只读）
-	 * @return { Boolean } 如果为true,则 resolve, 否则 reject
-	 */
-	validateStatus(statusCode) {
-		return statusCode === 200;
-	}
-
-	/**
-	 * @Function
-	 * @param {Request~setConfigCallback} f - 设置全局默认配置
-	 */
 	setConfig(f) {
 		this.config = f(this.config);
 	}
 
 
 	async request(options = {}) {
-		options.baseUrl = this.config.baseUrl;
-		options.dataType = options.dataType || this.config.dataType;
-		// #ifndef MP-ALIPAY || APP-PLUS
-		options.responseType = options.responseType || this.config.responseType;
-		// #endif
-		// #ifdef MP-ALIPAY
-		options.timeout = options.timeout || this.config.timeout;
-		// #endif
-		options.url = options.url || '';
-		options.data = options.data || {};
-		options.params = options.params || {};
 		options.header = options.header || this.config.header;
-		options.method = options.method || this.config.method;
-		options.custom = { ...this.config.custom, ...(options.custom || {}) };
 		// #ifdef APP-PLUS
 		options.sslVerify =
 			options.sslVerify === undefined
 				? this.config.sslVerify
 				: options.sslVerify;
 		// #endif
-		options.getTask = options.getTask || this.config.getTask;
 		return new Promise((resolve, reject) => {
-			let next = true;
-			const cancel = (t = 'handle cancel', config = options) => {
-				const err = {
-					errMsg: t,
-					config: config
-				};
-				reject(err);
-				next = false;
-			};
-
-			const handleRe = { ...this.requestBeforeFun(options, cancel) };
-			if (!next) return;
-			const requestTask = uni.request({
+			 uni.request({
 				url: options.url,
 				data: options.data,
 				header: options.header,
 				method: options.method,
 				sslVerify: options.sslVerify,
 				complete: response => {
-					response.config = handleRe;
-					if (this.validateStatus(response.statusCode)) {
+
+					if (response.statusCode===200) {
 						// 成功
 						response = this.requestComFun(response);
 						resolve(response);
@@ -144,9 +71,7 @@ export default class Request {
 					}
 				}
 			});
-			if (handleRe.getTask) {
-				handleRe.getTask(requestTask, handleRe);
-			}
+
 		});
 	}
 
@@ -190,7 +115,5 @@ export default class Request {
 			...options
 		});
 	}
-
 	// #endif
-
 }
